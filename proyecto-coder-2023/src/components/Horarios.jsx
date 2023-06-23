606d461055cb17679f549b9b636a49fc28f4ff61
Alexis Szerman <alexszer1986@gmail.com>
2023-06-23 17:58:42 -0300

    empezando con nosotros, mapa y horarios

--- proyecto-coder-2023/src/components/Horarios.jsx
+++ proyecto-coder-2023/src/components/Horarios.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import { Container } from "react-bootstrap";
 
 const Horarios = () => {
   return (
-    <div>Horarios</div>
-  )
-}
+    <Container className="bg-horarios py-4">
+      <div className="parent">
+        <div className="div1">
+          <div className="title-horarios">
+            <div className="d-flex flex-column fw-bold custom-gap">
+              <div className="fs-1">HO</div>
+              <div className="fs-1">RA</div>
+              <div className="fs-1">RIOS</div>
+            </div>
+          </div>
+        </div>
+        <div className="div2">
+          <span className="ms-4">Lunes</span>
+          <div className="div-detail">
+          <p className="text-center fw-bold">9:00 a 13:00</p>
+          <p className="text-center fw-bold">17:00 a 21:00</p>
+          </div>
 
-export default Horarios
+        </div>
+        <div className="div3">
+          <span className="ms-4">Martes</span>
+          <p className="text-center fw-bold div-detail">9:00 a 13:00</p>
+        </div>
+        <div className="div4">
+          <span className="ms-4">Miércoles</span>
+          <p className="text-center fw-bold div-detail">9:00 a 13:00</p>
+        </div>
+        <div className="div5">
+          <span className="ms-4">Jueves</span>
+          <div className="div-detail">
+          <p className="text-center fw-bold">9:00 a 13:00</p>
+          <p className="text-center fw-bold">17:00 a 21:00</p>
+          </div>
+
+        </div>
+        <div className="div6">
+          <span className="ms-4">Viernes</span>
+          <div className="div-detail">
+          <p className="text-center fw-bold">9:00 a 13:00</p>
+          <p className="text-center fw-bold">17:00 a 21:00</p>
+          </div>
+        </div>
+        <div className="div7">
+          <span className="ms-4">Sábados y Feriados</span>
+          <p className="text-center fw-bold div-detail">9:00 a 13:00</p>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Horarios;
--- proyecto-coder-2023/src/components/Mapa.jsx
+++ proyecto-coder-2023/src/components/Mapa.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Container} from "react-bootstrap";
 
-const Mapa = () => {
+const googleMapsEmbedCode =
+  '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d602.2009550003128!2d-64.13928175761968!3d-31.371035376399405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x943297704235155b%3A0x18f785ebf4746fe2!2sFERIA%20AMERICANA%20HORMIGAS%20COLORADAS!5e0!3m2!1ses-419!2sar!4v1687128478032!5m2!1ses-419!2sar" width="1000" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+
+function Mapa() {
   return (
-    <div>Mapa</div>
-  )
+    <Container className="mt-4">
+      <section>
+        <p className="fs-3">
+          Dónde <span className="fs-3" >encontrarnos</span>
+        </p>
+        <p className="mt-2 fs-6">Fernando Abramo 2365, X5012. Ciudad de Córdoba.</p>
+
+        <div className="d-flex align-items-center">
+          <img className="logo-whatsapp" src="../assets/img/whatsapp.png" alt="" />
+
+          <p className="fs-6">+54 9 351 742-4712</p>
+        </div>
+
+        <div dangerouslySetInnerHTML={{ __html: googleMapsEmbedCode }} />
+      </section>
+    </Container>
+  );
 }
 
-export default Mapa
+export default Mapa;
+
