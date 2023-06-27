3b850c35cffb8c2d78eac159808be4b6fd81bebe
Alexis Szerman <alexszer1986@gmail.com>
2023-06-27 11:48:44 -0300

    puliendo estilos generales 002

--- proyecto-coder-2023/src/components/Footer.jsx
+++ proyecto-coder-2023/src/components/Footer.jsx
@@ -2,19 +2,36 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="mt-4">
-    <img src="assets/img/footer.jpg" alt="footer" className="background-image"/>
-    <div className="footer-content">
-      <div className="left-container">
-        <div className="left-text mt-4"> <span>Reciclá</span> tu ropa y dale una nueva vida, ayudando al <span>ecosistema</span>  y
-            vistiendo a quienes más lo <span>necesitan</span></div>
+    <>
+      <footer className="mt-4">
+        <img
+          src="assets/img/footer.jpg"
+          alt="footer"
+          className="background-image"
+        />
+        <div className="footer-content">
+          <div className="left-container">
+            <div className="left-text mt-4">
+              {" "}
+              <span>Reciclá</span> tu ropa y dale una nueva vida, ayudando al{" "}
+              <span>ecosistema</span> y vistiendo a quienes más lo{" "}
+              <span>necesitan</span>
+            </div>
+          </div>
+          <div className="right-logo">
+            <img
+              src="assets/img/hormigas-coloradas-logo-sin-background.png"
+              alt="Logo"
+            />
+          </div>
+        </div>
+      </footer>
+
+      <div className="d-flex m-3 justify-content-center">
+        <p className="mt-2" >Hormigas Coloradas 2023</p>
       </div>
-      <div className="right-logo"><img src="assets/img/hormigas-coloradas-logo-sin-background.png" alt="Logo"/></div>
-    </div>
-  </footer>
-  
+    </>
   );
 };
 
 export default Footer;
-
--- proyecto-coder-2023/src/components/Mapa.jsx
+++ proyecto-coder-2023/src/components/Mapa.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container} from "react-bootstrap";
 
 const googleMapsEmbedCode =
-  '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d602.2009550003128!2d-64.13928175761968!3d-31.371035376399405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x943297704235155b%3A0x18f785ebf4746fe2!2sFERIA%20AMERICANA%20HORMIGAS%20COLORADAS!5e0!3m2!1ses-419!2sar!4v1687128478032!5m2!1ses-419!2sar" width="1000" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+  '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d602.2009550003128!2d-64.13928175761968!3d-31.371035376399405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x943297704235155b%3A0x18f785ebf4746fe2!2sFERIA%20AMERICANA%20HORMIGAS%20COLORADAS!5e0!3m2!1ses-419!2sar!4v1687128478032!5m2!1ses-419!2sar" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
 
 function Mapa() {
   return (
@@ -13,7 +13,7 @@ function Mapa() {
         </p>
         <p className="mt-2 fs-6">Fernando Abramo 2365, X5012. Ciudad de Córdoba.</p>
 
-        <div className="d-flex align-items-center">
+        <div className="d-flex align-items-center mb-4">
           <img className="logo-whatsapp" src="../assets/img/whatsapp.png" alt="" />
 
           <p className="fs-6">+54 9 351 742-4712</p>
