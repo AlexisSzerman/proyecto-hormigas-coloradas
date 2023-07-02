27bbee4bc689b35c6fa69643a4358f8ff94a46da
martin posniak <martin.posniak@hotmail.com>
2023-07-02 17:54:28 -0300

    proyecto modificado

--- proyecto-coder-2023/src/components/Logo.jsx
+++ proyecto-coder-2023/src/components/Logo.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
+import React from "react";
 import hormigaLogo from "/assets/img/hormigas-coloradas-logo-version-2.png";
 import facebookLogo from "/assets/img/logo-de-facebook.png";
-import Carousel from './Carousel';
+import Carousel from "./Carousel";
 
 const Logo = () => {
   return (
     <>
-    <div className='container'>
+      <div className="container">
         <div className="welcome">
-            <img className="hormigaFoto" src={hormigaLogo} alt="" />
+          <img className="hormigaFoto" src={hormigaLogo} alt="" />
         </div>
-        <div className='ropaCircular'>
+        <div className="ropaCircular">
           <h2>Ropa circular & objetos</h2>
         </div>
-        <div className='seguinos'>
-            <img className='facebookLogo' src={facebookLogo} alt="" /> &nbsp;Seguinos en <strong> &nbsp;Facebook</strong>
-          </div>
-    </div>
+        <div className="seguinos">
+          <a href="https://www.facebook.com/hormigas.coloradas" target="_blank" className="facebookTexto">
+            <img className="facebookLogo" src={facebookLogo} /> &nbsp;Seguinos en <strong>Facebook</strong>
+          </a>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
